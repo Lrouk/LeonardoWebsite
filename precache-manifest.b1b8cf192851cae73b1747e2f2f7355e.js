dd268ae90b50ca7d9820b65b513406921b8aa155
lrouk <leonidrouk10@gmail.com>
2023-06-26 20:27:53 +0300

    Updates

---
+++ precache-manifest.b1b8cf192851cae73b1747e2f2f7355e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "af2a6c457bc0bce98455",
+    "url": "/LeonardoWebsite/static/css/main.b90ed315.chunk.css"
+  },
+  {
+    "revision": "af2a6c457bc0bce98455",
+    "url": "/LeonardoWebsite/static/js/main.af2a6c45.chunk.js"
+  },
+  {
+    "revision": "b66268068b0e064afcc3",
+    "url": "/LeonardoWebsite/static/css/1.4099ed72.chunk.css"
+  },
+  {
+    "revision": "b66268068b0e064afcc3",
+    "url": "/LeonardoWebsite/static/js/1.b6626806.chunk.js"
+  },
+  {
+    "revision": "99b4621e74b835c22c55",
+    "url": "/LeonardoWebsite/static/js/runtime~main.99b4621e.js"
+  },
+  {
+    "revision": "ab127e00c8e887e4226fd3f4e713f053",
+    "url": "/LeonardoWebsite/static/media/gallery01.ab127e00.jpg"
+  },
+  {
+    "revision": "4722e20d29f8aebec2d04f2bff206982",
+    "url": "/LeonardoWebsite/static/media/gallery02.4722e20d.jpg"
+  },
+  {
+    "revision": "9400e0c8db7ec20e8d60a4a6e4a3e766",
+    "url": "/LeonardoWebsite/static/media/gallery03.9400e0c8.jpg"
+  },
+  {
+    "revision": "e15f0ff8f44c296ad722f4fe9ca5e8ab",
+    "url": "/LeonardoWebsite/static/media/gallery04.e15f0ff8.jpg"
+  },
+  {
+    "revision": "f36172fc165e38a6c1225d3981a41396",
+    "url": "/LeonardoWebsite/static/media/Leonardo_topLogo.f36172fc.png"
+  },
+  {
+    "revision": "6119706654ffecde086978113e6558f7",
+    "url": "/LeonardoWebsite/static/media/aboutUs.61197066.jpg"
+  },
+  {
+    "revision": "15cc730d5805f4e8fa75135e8314979f",
+    "url": "/LeonardoWebsite/static/media/Leonardo_footer.15cc730d.png"
+  },
+  {
+    "revision": "ec42111c3a0eb93bbd665a4a98a05ccc",
+    "url": "/LeonardoWebsite/static/media/awards.ec42111c.jpg"
+  },
+  {
+    "revision": "b99fb64b7473fc2525b2ce4f144bb946",
+    "url": "/LeonardoWebsite/static/media/menuDrinks.b99fb64b.jpg"
+  },
+  {
+    "revision": "a4fa6e18b2f84e3b97db4b6c63351b41",
+    "url": "/LeonardoWebsite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2f55da163e65edd52af401335ac4eb3.js"
+  "/LeonardoWebsite/precache-manifest.b1b8cf192851cae73b1747e2f2f7355e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/LeonardoWebsite/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
