70bb97162d3bae749db6237ed5e72f8dc3244d51
lrouk <leonidrouk10@gmail.com>
2023-06-26 20:13:47 +0300

    Updates

---
+++ precache-manifest.e2f55da163e65edd52af401335ac4eb3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "af2a6c457bc0bce98455",
+    "url": "/static/css/main.b90ed315.chunk.css"
+  },
+  {
+    "revision": "af2a6c457bc0bce98455",
+    "url": "/static/js/main.af2a6c45.chunk.js"
+  },
+  {
+    "revision": "b66268068b0e064afcc3",
+    "url": "/static/css/1.4099ed72.chunk.css"
+  },
+  {
+    "revision": "b66268068b0e064afcc3",
+    "url": "/static/js/1.b6626806.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab127e00c8e887e4226fd3f4e713f053",
+    "url": "/static/media/gallery01.ab127e00.jpg"
+  },
+  {
+    "revision": "4722e20d29f8aebec2d04f2bff206982",
+    "url": "/static/media/gallery02.4722e20d.jpg"
+  },
+  {
+    "revision": "9400e0c8db7ec20e8d60a4a6e4a3e766",
+    "url": "/static/media/gallery03.9400e0c8.jpg"
+  },
+  {
+    "revision": "e15f0ff8f44c296ad722f4fe9ca5e8ab",
+    "url": "/static/media/gallery04.e15f0ff8.jpg"
+  },
+  {
+    "revision": "f36172fc165e38a6c1225d3981a41396",
+    "url": "/static/media/Leonardo_topLogo.f36172fc.png"
+  },
+  {
+    "revision": "6119706654ffecde086978113e6558f7",
+    "url": "/static/media/aboutUs.61197066.jpg"
+  },
+  {
+    "revision": "15cc730d5805f4e8fa75135e8314979f",
+    "url": "/static/media/Leonardo_footer.15cc730d.png"
+  },
+  {
+    "revision": "ec42111c3a0eb93bbd665a4a98a05ccc",
+    "url": "/static/media/awards.ec42111c.jpg"
+  },
+  {
+    "revision": "b99fb64b7473fc2525b2ce4f144bb946",
+    "url": "/static/media/menuDrinks.b99fb64b.jpg"
+  },
+  {
+    "revision": "7e8356f037b788d41b6bb3ab7dea9431",
+    "url": "/index.html"
+  }
+];
